08e0ae8901bc2128f32e610eb8a5fcbce5b90723
Vijaya Lakshmi <24jr1a05eu@gmail.com>
2025-12-22 20:37:18 +0530

    Add operators_questions.py (operator practice problems)

---
+++ basics/basics/operators_questions.py
@@ -0,0 +1,31 @@
+#  Arithmetic operators
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+
+
+#  Comparison operators
+num = int(input("Enter a number: "))
+if num > 50:
+    print("Greater than 50")
+else:
+    print("Less than or equal to 50")
+
+
+
+#  Logical operators
+if num > 10 and num < 100:
+    print("Number is between 10 and 100")
+
+
+
+
+#  String multiplication
+a = "10"
+b = 5
+print(a * b)
